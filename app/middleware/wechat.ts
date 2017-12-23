6322d3da329db05485d4c859fedef045b92d0656
方正 <215566435@qq.com>
2017-12-24 12:36:10 +1300

    ➕解析中间件

--- app/middleware/wechat.ts
+++ app/middleware/wechat.ts
@@ -5,6 +5,13 @@ module.exports = (options: any) => {
     return async function wechat(ctx: Context, next: any) {
         if (ctx.request.headers['content-type'] === 'text/xml') {
             console.log(ctx.request.body);
+            let buf = ''
+            ctx.req.on('data', (chunk) => {
+                buf += chunk
+            })
+            ctx.req.on('end', () => {
+                console.log('解析完毕', buf)
+            })
         }
         await next();
     }
--- build/app/middleware/wechat.js
+++ build/app/middleware/wechat.js
@@ -4,6 +4,13 @@ module.exports = (options) => {
     return async function wechat(ctx, next) {
         if (ctx.request.headers['content-type'] === 'text/xml') {
             console.log(ctx.request.body);
+            let buf = '';
+            ctx.req.on('data', (chunk) => {
+                buf += chunk;
+            });
+            ctx.req.on('end', () => {
+                console.log('解析完毕', buf);
+            });
         }
         await next();
     };
