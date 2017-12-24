015467054ce7c48dfe88e12f5bf7a7d3ff69cd1b
方正 <215566435@qq.com>
2017-12-24 14:15:54 +1300

    修改contenttype.

--- app/controller/home.ts
+++ app/controller/home.ts
@@ -15,10 +15,10 @@ class HomeController extends Controller {
         ${foodspec.pro}\n
         注意：如果出现0g的数字，代表这种元素在此类物品里量太少，忽略不计`;
 
-        
+
         this.ctx.body = this.ctx.helper.utils.returnWechatMsg(toUser, msg)
         console.log(this.ctx.body)
-        this.ctx.set('Content-Type', 'text/xml;charset=utf-8');
+        this.ctx.set('Content-Type', 'text/xml');
     }
 }
 
