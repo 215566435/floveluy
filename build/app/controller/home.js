4deea3916ee8ac802c2041ab79633a467f9454e4
方正 <215566435@qq.com>
2017-12-24 15:27:21 +1300

    完成食物

--- build/app/controller/home.js
+++ build/app/controller/home.js
@@ -7,7 +7,7 @@ class HomeController extends egg_1.Controller {
         let toUser = xmlbody['xml']['FromUserName'];
         let Content = xmlbody['xml']['Content'];
         const foodspec = await this.ctx.service.food.calulate(Content.split(" "));
-        const msg = `「${foodspec.title}」\n${foodspec.cal}\n${foodspec.carbs}\n${foodspec.fat}\n${foodspec.pro}\n注意：如果出现0g的数字，代表这种元素在此类物品里量太少，忽略不计`;
+        const msg = `「${foodspec.title}」\n${foodspec.cal}\n${foodspec.carbs}\n${foodspec.fat}\n${foodspec.pro}\n\n注意：如果出现0g的数字，代表这种元素在此类物品里量太少，忽略不计`;
         this.ctx.body = this.ctx.helper.utils.returnWechatMsg(toUser, msg);
         this.ctx.set('Content-Type', 'text/plain; charset=utf-8');
     }
