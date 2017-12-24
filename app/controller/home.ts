d61bd09df938919f2dfab2a08b683bb7bb33142b
方正 <215566435@qq.com>
2017-12-24 14:12:08 +1300

    type修复text/xml

--- app/controller/home.ts
+++ app/controller/home.ts
@@ -18,7 +18,7 @@ class HomeController extends Controller {
         
         this.ctx.body = this.ctx.helper.utils.returnWechatMsg(toUser, msg)
         console.log(this.ctx.body)
-        this.ctx.set('Content-Type', 'text/html;charset=utf-8');
+        this.ctx.set('Content-Type', 'text/xml;charset=utf-8');
     }
 }
 
