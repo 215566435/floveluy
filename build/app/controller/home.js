076a8289d57d9dfe9988f219b542691ca97dfb3a
方正 <215566435@qq.com>
2017-12-24 14:21:34 +1300

    修改

--- build/app/controller/home.js
+++ build/app/controller/home.js
@@ -7,12 +7,7 @@ class HomeController extends egg_1.Controller {
         let toUser = xmlbody['xml']['FromUserName'];
         let Content = xmlbody['xml']['Content'];
         const foodspec = await this.ctx.service.food.calulate(Content.split(","));
-        const msg = `「${foodspec.title}」
-        ${foodspec.cal}
-        ${foodspec.carbs}
-        ${foodspec.fat}
-        ${foodspec.pro}\n
-        注意：如果出现0g的数字，代表这种元素在此类物品里量太少，忽略不计`;
+        const msg = `「${foodspec.title}」${foodspec.cal}${foodspec.carbs}${foodspec.fat}${foodspec.pro}注意：如果出现0g的数字，代表这种元素在此类物品里量太少，忽略不计`;
         this.ctx.body = this.ctx.helper.utils.returnWechatMsg(toUser, msg);
         console.log(this.ctx.body);
         // this.ctx.set('Content-Type', 'text/xml');
