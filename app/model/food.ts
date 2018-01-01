93af2fb3b7a91691fc87cd7b13c512b1bdcb46ce
方正 <215566435@qq.com>
2018-01-01 10:28:20 +0800

    ➕构建model

--- app/model/food.ts
+++ app/model/food.ts
@@ -2,15 +2,16 @@ import { Application } from 'egg';
 import { ModelDefine } from './ModelDefine';
 
 module.exports = (app: Application) => {
-    const { STRING, DOUBLE, TIME } = app.Sequelize;
+    const { STRING, TIME } = app.Sequelize;
 
     const food = ModelDefine(app, 'food', {
         sets: STRING(20),
         weight: STRING(40),
         time: TIME,
         surface: STRING(255),
-        name: DOUBLE
+        name: STRING(40)
     });
 
     return food;
 };
+
--- build/app/model/food.js
+++ build/app/model/food.js
@@ -2,13 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const ModelDefine_1 = require("./ModelDefine");
 module.exports = (app) => {
-    const { STRING, DOUBLE, TIME } = app.Sequelize;
+    const { STRING, TIME } = app.Sequelize;
     const food = ModelDefine_1.ModelDefine(app, 'food', {
         sets: STRING(20),
         weight: STRING(40),
         time: TIME,
         surface: STRING(255),
-        name: DOUBLE
+        name: STRING(40)
     });
     return food;
 };
